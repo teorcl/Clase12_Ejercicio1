d032a23f358ba9b1e76cb80eadee33a2a49fb98a
Teo <teo.calle.lara@gmail.com>
2023-01-02 10:46:35 -0500

    Initial Commit

---
+++ Clase12_Ejercicio1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clase12_Ejercicio1
+//
+//  Created by ADMIN on 2/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
